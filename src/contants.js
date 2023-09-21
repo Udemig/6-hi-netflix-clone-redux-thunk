34000397c19827face49154fbc4bdf835b2e2465
fhasancelik <hasancelikjob@gmail.com>
2023-09-22 02:32:19 +0300

    de

--- src/contants.js
+++ src/contants.js
@@ -2,7 +2,7 @@ export const options = {
     params: {language: 'en-US', page: '1'},
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTcyZTczMmQzYzMyMzNhMzljZGMyOTM1ZDJjZTg1NiIsInN1YiI6IjY0YmUzNDM3YWM2Yzc5MDhlMDg2Zjc5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9DvE6PNWCjKXr6sh8hKF1j_kx7_tcab1JUfg_dU6PRk'
+      Authorization: 'API Key Girin'
     }
   };
 
